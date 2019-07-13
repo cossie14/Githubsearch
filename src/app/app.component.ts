e136fd2e02dd93f9c0e022080f626440309c1390
sylviah <sylviah.rutto@gmail.com>
2019-07-13 18:56:11 +0300

    updated HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Githubsearch } from './githubsearch';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubsearchComponent } from './githubsearch/githubsearch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubsearchComponent
   ],
   imports: [
     BrowserModule,
